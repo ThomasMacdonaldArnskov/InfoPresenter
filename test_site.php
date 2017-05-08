6ff19a67dcdfdd8b0480de4ff07e2430c8822e4a
Thomas Laurits Macdonald Arnskov <thomas.macdonald.arnskov@gmail.com>
2017-05-08 20:31:03 +0200

    Missing files

--- test_site.php
+++ test_site.php
@@ -1,75 +1,215 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php session_start(); ?>
+
+<!doctype html>
+<html>
+
+<?php
+
+include('includes/header.php');
+include('includes/login_script.php');
+
+//If a user is logged in he see's the corresponding navigational menu, otherwise the regular
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    include('includes/menuMember.php');
+} else {
+    include('includes/menu.php');
+}
+
+//$presname = $_POST['pres_name'];
+//$username = $_SESSION['username'];
+
+require_once('includes/connection.php');
+$db_host = getDbHost();
+$db_password = getDbPassword();
+$db_username = getDbUser();
+$db_dbname = getDbDatabase();
+$conn = new mysqli($db_host, $db_username, $db_password, $db_dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `tlma_pres`";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$field_cnt = $result->field_count;
+$field_cnt=$field_cnt-2;
+$row_cnt = $result->num_rows;
+
+
+?>
+
 <head>
-    <title>Bootstrap image Overlay</title>
-    <meta content="width=device-width, initialscale=1" name="viewport">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.j" type="text/javascript">
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript">
-    </script>
+    <title>Bar Chart</title>
+    <br/><br/><br/><br/>
+    <script src="vendor/nnnick/chartjs/dist/Chart.bundle.js"></script>
+    <script src="vendor/nnnick/chartjs/samples/utils.js"></script>
     <style>
-        .caption {
-            width:100%;
-            bottom: .3rem;
-            position: absolute;
-            background:#000;
-            background: -webkit-linear-gradient(bottom, #000 40%, rgba(0, 0, 0, 0) 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
-            background: -moz-linear-gradient(bottom, #000 40%, rgba(0, 0, 0, 0) 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
-            background: -o-linear-gradient(bottom, #000 40%, rgba(0, 0, 0, 0) 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
-            background: linear-gradient(to top, #000 40%, rgba(0, 0, 0, 0) 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
-        }
-
-        .thumbnail {
-            border: 0 none;
-            box-shadow: none;
-            margin:0;
-            padding:0;
-        }
-
-        .caption h4 {
-            color: #fff;
-            -webkit-font-smoothing: antialiased;
+        canvas {
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
         }
     </style>
 </head>
+
 <body>
-<div class="container" style="margin:20px;">
-    <div class=" col-sm-12 row">
-        <div class="col-sm-4">
-            <div class="col-sm-12 thumbnail text-center">
-                <img alt="" class="img-responsive" src=
-                "http://www.wallpapereast.com/static/images/6801692-lovely-nature-wallpaper.jpg">
-
-                <div class="caption">
-                    <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="col-sm-12 thumbnail text-center">
-                <img alt="" class="img-responsive" src=
-                "http://www.wallpapereast.com/static/images/6801692-lovely-nature-wallpaper.jpg">
-
-                <div class="caption">
-                    <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-4">
-            <div class="col-sm-12 thumbnail text-center">
-                <img alt="" class="img-responsive" src=
-                "http://www.wallpapereast.com/static/images/6801692-lovely-nature-wallpaper.jpg">
-
-                <div class="caption">
-                    <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="container" style="width: 75%;">
+    <canvas id="canvas"></canvas>
 </div>
+<script>
+    //var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var color = Chart.helpers.color;
+    var barChartData = {
+    <?php
+    echo 'labels: ["", "", "", "", "", "", ""],';
+    echo 'datasets: [{';
+    for ($i = 0; $i < $field_cnt; $i++) {
+        $index = $i+2;
+        echo 'label: "' . $i . '",';
+        echo 'backgroundColor: color(window.chartColors.red).alpha(1).rgbString(),';
+        echo 'borderColor: window.chartColors.red,';
+        echo 'borderWidth: 1,';
+        echo 'data: [';
+        echo $row["$index"];
+        echo ']';
+        echo '},';
+
+        if ($i != $field_cnt-1) {
+            echo '{';
+        }
+    }
+    ?>
+
+
+    //      labels: ["January", "February", "March", "April", "May", "June", "July"],
+    //      datasets: [{
+    //          label: 'Dataset 1',
+    //          backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+    //          borderColor: window.chartColors.red,
+    //          borderWidth: 1,
+    //          data: [
+    //              randomScalingFactor(),
+    //              randomScalingFactor(),
+    //              randomScalingFactor(),
+    //            randomScalingFactor(),
+    //            randomScalingFactor(),
+    //            randomScalingFactor(),
+    //            randomScalingFactor()
+    //          ]},
+
+    //    {
+    //      label: 'Dataset 2',
+    //          backgroundColor
+    //  :
+    //      color(window.chartColors.blue).alpha(1).rgbString(),
+    //          borderColor
+    //  :
+    //      window.chartColors.blue,
+    //          borderWidth
+    //  :
+    //      1,
+    //          data
+    //  :
+    //      [
+    //          randomScalingFactor(),
+    //          randomScalingFactor(),
+    //          randomScalingFactor(),
+    //          randomScalingFactor(),
+    //          randomScalingFactor(),
+    //          randomScalingFactor(),
+    //          randomScalingFactor()
+    //      ]
+    //  }
+    ]
+
+    }
+    ;
+
+    window.onload = function () {
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Bar Chart'
+                }
+            }
+        });
+
+    };
+
+    document.getElementById('randomizeData').addEventListener('click', function () {
+        var zero = Math.random() < 0.2 ? true : false;
+        barChartData.datasets.forEach(function (dataset) {
+            dataset.data = dataset.data.map(function () {
+                return zero ? 0.0 : randomScalingFactor();
+            });
+
+        });
+        window.myBar.update();
+    });
+
+    var colorNames = Object.keys(window.chartColors);
+    document.getElementById('addDataset').addEventListener('click', function () {
+        var colorName = colorNames[barChartData.datasets.length % colorNames.length];
+        ;
+        var dsColor = window.chartColors[colorName];
+        var newDataset = {
+            label: 'Dataset ' + barChartData.datasets.length,
+            backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+            borderColor: dsColor,
+            borderWidth: 1,
+            data: []
+        };
+
+        for (var index = 0; index < barChartData.labels.length; ++index) {
+            newDataset.data.push(randomScalingFactor());
+        }
+
+        barChartData.datasets.push(newDataset);
+        window.myBar.update();
+    });
+
+    document.getElementById('addData').addEventListener('click', function () {
+        if (barChartData.datasets.length > 0) {
+            var month = MONTHS[barChartData.labels.length % MONTHS.length];
+            barChartData.labels.push(month);
+
+            for (var index = 0; index < barChartData.datasets.length; ++index) {
+                //window.myBar.addData(randomScalingFactor(), index);
+                barChartData.datasets[index].data.push(randomScalingFactor());
+            }
+
+            window.myBar.update();
+        }
+    });
+
+    document.getElementById('removeDataset').addEventListener('click', function () {
+        barChartData.datasets.splice(0, 1);
+        window.myBar.update();
+    });
+
+    document.getElementById('removeData').addEventListener('click', function () {
+        barChartData.labels.splice(-1, 1); // remove the label first
+
+        barChartData.datasets.forEach(function (dataset, datasetIndex) {
+            dataset.data.pop();
+        });
+
+        window.myBar.update();
+    });
+</script>
+
+<?php
+
+?>
 </body>
+
 </html>

d9a6c510de241fc40c1922c7fcac9f528c625ef1
Thomas Laurits Macdonald Arnskov <thomas.macdonald.arnskov@gmail.com>
2017-05-09 15:12:43 +0200

    Sort of works

--- test_site.php
+++ test_site.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 
-<!doctype html>
-<html>
+    <!doctype html>
+    <html>
 
 <?php
 
@@ -16,7 +16,11 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 }
 
 //$presname = $_POST['pres_name'];
+$presname = "pres";
 //$username = $_SESSION['username'];
+$username = "tlma";
+
+$non_value_fields = 2;
 
 require_once('includes/connection.php');
 $db_host = getDbHost();
@@ -29,187 +33,155 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM `tlma_pres`";
+$presname_db = str_replace(' ', '', $presname);
+$presname_db = $username . "_" . $presname_db;
+
+$rowcounterfetch = "SELECT * FROM `$presname_db`";
+$rowresult = $conn->query($rowcounterfetch);
+$row_cnt = $rowresult->num_rows;
+$rowresult->close();
+
+$graphcolor = array("backgroundColor: color(window.chartColors.red).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.orange).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.yellow).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.green).alpha(1).rgbString(),");
+$bordercolor = array("borderColor: window.chartColors.red,", "borderColor: window.chartColors.orange,", "borderColor: window.chartColors.yellow,", "borderColor: window.chartColors.green,");
+
+$sql = "SELECT * FROM `$presname_db` WHERE id = '1'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$field_cnt = $result->field_count;
-$field_cnt=$field_cnt-2;
-$row_cnt = $result->num_rows;
+$counter_array = array_filter($row, 'strlen');
 
-
+$field_cnt = count($counter_array);
+$field_cnt = $field_cnt - $non_value_fields;
+$result->close();
 ?>
 
-<head>
-    <title>Bar Chart</title>
-    <br/><br/><br/><br/>
-    <script src="vendor/nnnick/chartjs/dist/Chart.bundle.js"></script>
-    <script src="vendor/nnnick/chartjs/samples/utils.js"></script>
-    <style>
-        canvas {
-            -moz-user-select: none;
-            -webkit-user-select: none;
-            -ms-user-select: none;
-        }
-    </style>
-</head>
-
-<body>
-<div id="container" style="width: 75%;">
-    <canvas id="canvas"></canvas>
-</div>
-<script>
-    //var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var color = Chart.helpers.color;
-    var barChartData = {
-    <?php
-    echo 'labels: ["", "", "", "", "", "", ""],';
-    echo 'datasets: [{';
-    for ($i = 0; $i < $field_cnt; $i++) {
-        $index = $i+2;
-        echo 'label: "' . $i . '",';
-        echo 'backgroundColor: color(window.chartColors.red).alpha(1).rgbString(),';
-        echo 'borderColor: window.chartColors.red,';
-        echo 'borderWidth: 1,';
-        echo 'data: [';
-        echo $row["$index"];
-        echo ']';
-        echo '},';
-
-        if ($i != $field_cnt-1) {
-            echo '{';
-        }
-    }
-    ?>
-
-
-    //      labels: ["January", "February", "March", "April", "May", "June", "July"],
-    //      datasets: [{
-    //          label: 'Dataset 1',
-    //          backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-    //          borderColor: window.chartColors.red,
-    //          borderWidth: 1,
-    //          data: [
-    //              randomScalingFactor(),
-    //              randomScalingFactor(),
-    //              randomScalingFactor(),
-    //            randomScalingFactor(),
-    //            randomScalingFactor(),
-    //            randomScalingFactor(),
-    //            randomScalingFactor()
-    //          ]},
-
-    //    {
-    //      label: 'Dataset 2',
-    //          backgroundColor
-    //  :
-    //      color(window.chartColors.blue).alpha(1).rgbString(),
-    //          borderColor
-    //  :
-    //      window.chartColors.blue,
-    //          borderWidth
-    //  :
-    //      1,
-    //          data
-    //  :
-    //      [
-    //          randomScalingFactor(),
-    //          randomScalingFactor(),
-    //          randomScalingFactor(),
-    //          randomScalingFactor(),
-    //          randomScalingFactor(),
-    //          randomScalingFactor(),
-    //          randomScalingFactor()
-    //      ]
-    //  }
-    ]
-
-    }
-    ;
-
-    window.onload = function () {
-        var ctx = document.getElementById("canvas").getContext("2d");
-        window.myBar = new Chart(ctx, {
-            type: 'bar',
-            data: barChartData,
-            options: {
-                responsive: true,
-                legend: {
-                    position: 'top',
-                },
-                title: {
-                    display: true,
-                    text: 'Chart.js Bar Chart'
-                }
+    <head>
+        <script src="vendor/nnnick/chartjs/dist/Chart.bundle.js"></script>
+        <script src="vendor/nnnick/chartjs/samples/utils.js"></script>
+        <style>
+            canvas {
+                -moz-user-select: none;
+                -webkit-user-select: none;
+                -ms-user-select: none;
             }
-        });
-
-    };
-
-    document.getElementById('randomizeData').addEventListener('click', function () {
-        var zero = Math.random() < 0.2 ? true : false;
-        barChartData.datasets.forEach(function (dataset) {
-            dataset.data = dataset.data.map(function () {
-                return zero ? 0.0 : randomScalingFactor();
-            });
-
-        });
-        window.myBar.update();
-    });
-
-    var colorNames = Object.keys(window.chartColors);
-    document.getElementById('addDataset').addEventListener('click', function () {
-        var colorName = colorNames[barChartData.datasets.length % colorNames.length];
-        ;
-        var dsColor = window.chartColors[colorName];
-        var newDataset = {
-            label: 'Dataset ' + barChartData.datasets.length,
-            backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-            borderColor: dsColor,
-            borderWidth: 1,
-            data: []
-        };
-
-        for (var index = 0; index < barChartData.labels.length; ++index) {
-            newDataset.data.push(randomScalingFactor());
-        }
-
-        barChartData.datasets.push(newDataset);
-        window.myBar.update();
-    });
-
-    document.getElementById('addData').addEventListener('click', function () {
-        if (barChartData.datasets.length > 0) {
-            var month = MONTHS[barChartData.labels.length % MONTHS.length];
-            barChartData.labels.push(month);
-
-            for (var index = 0; index < barChartData.datasets.length; ++index) {
-                //window.myBar.addData(randomScalingFactor(), index);
-                barChartData.datasets[index].data.push(randomScalingFactor());
-            }
-
-            window.myBar.update();
-        }
-    });
-
-    document.getElementById('removeDataset').addEventListener('click', function () {
-        barChartData.datasets.splice(0, 1);
-        window.myBar.update();
-    });
-
-    document.getElementById('removeData').addEventListener('click', function () {
-        barChartData.labels.splice(-1, 1); // remove the label first
-
-        barChartData.datasets.forEach(function (dataset, datasetIndex) {
-            dataset.data.pop();
-        });
-
-        window.myBar.update();
-    });
-</script>
-
+        </style>
+    </head>
+    <body>
+    <br/><br/><br/><br/><br/>
+    <H6 class="text-center">Presentation title</H6>
+    <br/><br/><br/>
 <?php
+$num = 0;
 
-?>
+for ($i = 0; $i < $row_cnt; $i++) {
+    $idsrc = $i + 1;
+    $sql = "SELECT * FROM `$presname_db` WHERE id = '$idsrc'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $counter_array = array_filter($row, 'strlen');
+
+    $field_cnt = count($counter_array);
+    $field_cnt = $field_cnt - $non_value_fields;
+    $result->close();
+
+    if ($num % 2 == 0) {
+        echo '<div class="container-fluid bg-3 text-center">
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-5 text-right">
+            <h1>';
+        echo $row["1"];
+        echo '</h1>
+            <P>FILMED IN NEVADA</P><br/>
+            <span class="glyphicon glyphicon-search" aria-hidden="true" style="font-size:250%;"></span>
+            <div id="container">
+                <canvas id="chart' . $i . '"></canvas>
+            </div>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, donec volutpat egestas eget egestas sed porttitor, nulla nonummy nec ut,
+                nulla vel ultricies amet, ac lectus consequat velit tempor, quam vestibulum vitae. Sit nullam. Veniam
+                justo nunc porttitor magna sed ante, mi nulla orci odio eros id nullam, cras bibendum feugiat. At
+                vestibulum donec massa purus ut augue, mauris sed magna ipsum omnis amet, vestibulum laoreet dolor,
+                aliquam ipsum risus tempus ut eros, fusce purus urna ullamcorper lobortis</p>
+
+        </div>';
+    }
+
+    if ($num % 2 == 1) {
+        echo '<div class="col-sm-5 text-right">
+            <h1>';
+        echo $row["1"];
+        echo '</h1>
+            <P>FILMED IN NEVADA</P><br/>
+            <span class="glyphicon glyphicon-search" aria-hidden="true" style="font-size:250%;"></span>
+            <div id="container">
+                <canvas id="chart' . $i . '"></canvas>
+            </div>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, donec volutpat egestas eget egestas sed porttitor, nulla nonummy nec ut,
+                nulla vel ultricies amet, ac lectus consequat velit tempor, quam vestibulum vitae. Sit nullam. Veniam
+                justo nunc porttitor magna sed ante, mi nulla orci odio eros id nullam, cras bibendum feugiat. At
+                vestibulum donec massa purus ut augue, mauris sed magna ipsum omnis amet, vestibulum laoreet dolor,
+                aliquam ipsum risus tempus ut eros, fusce purus urna ullamcorper lobortis</p>
+
+        </div>
+        <div class="col-sm-1"></div> 
+    </div>
+</div>';
+
+
+    $num++;
+}
+
+    echo '<script>';
+
+        echo 'var color = Chart.helpers.color;';
+
+        echo 'var barChartData'.$i.' = {';
+
+        echo 'labels: [""],';
+        echo 'datasets: [{';
+        for ($s = 0; $s < $field_cnt; $s++) {
+            $index = $s + 2;
+            echo 'label: "Row ' . $s . '",';
+            echo $graphcolor[$s % 4];
+            echo 'borderColor: window.chartColors.red,';
+            echo 'borderWidth: 0,';
+            echo 'data: [';
+            echo $row["$index"];
+            echo ']';
+            echo '},';
+
+            if ($s != $field_cnt - 1) {
+                echo '{';
+            }
+        }
+
+        echo ']';
+        echo '};';
+
+
+        echo 'var ctx = document.getElementById("chart'.$i.'");';
+        echo 'var chart'.$i.' = new Chart(ctx, {';
+        echo 'type: "bar",';
+            echo 'data: barChartData'.$i.',';
+            echo 'options: {';
+                echo 'scales: {';
+                    echo 'xAxes: [{';
+                    echo'    display: false';
+
+                    echo '}],';
+                    echo 'yAxes: [{';
+
+                        echo 'display: false';
+
+                    echo '}]';
+                echo '},';
+                echo 'legend: {';
+                    echo 'display: false';
+                echo '}';
+            echo '}';
+        echo '})';
+    echo'</script>'; } ?>
 </body>
 
 </html>
--- view_presentation.php
+++ view_presentation.php
@@ -1,85 +1,187 @@
 <?php session_start(); ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-
-    <!-- Include the headers and scripts-->
-    <?php
-    include('includes/header.php');
-    include('includes/login_script.php');
-
-    //If a user is logged in he see's the corresponding navigational menu, otherwise the regular
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-        include('includes/menuMember.php');
-    } else {
-        include('includes/menu.php');
-    }
-
-    $presname = $_POST['pres_name'];
-    $username = $_SESSION['username'];
-
-    ?>
-
-</head>
-<body>
-
-<br/><br/><br/><br/>
 
 <?php
 
-echo $username;
-echo "<br />";
-echo $presname;
-echo "<br />";
+include('includes/header.php');
+include('includes/login_script.php');
+
+//If a user is logged in he see's the corresponding navigational menu, otherwise the regular
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    include('includes/menuMember.php');
+} else {
+    include('includes/menu.php');
+}
+
+$presname = $_POST['pres_name'];
+//$presname = "pres";
+$username = $_SESSION['username'];
+//$username = "tlma";
+
+$non_value_fields = 2;
 
 require_once('includes/connection.php');
 $db_host = getDbHost();
 $db_password = getDbPassword();
 $db_username = getDbUser();
 $db_dbname = getDbDatabase();
-
 $conn = new mysqli($db_host, $db_username, $db_password, $db_dbname);
-
-$sql = "SELECT * FROM user_presentations_table WHERE `user` = '$username' AND `presentation_name` = '$presname'";
-$result = $conn->query($sql);
-
-while($row = $result->fetch_assoc()) {
-    echo $row["presentation_name"];
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-
-echo "<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />";
 $presname_db = str_replace(' ', '', $presname);
-$presname_db = $username."_".$presname_db;
-echo $presname_db;
-$content = "SELECT * FROM `$presname_db`";
-$result_db = $conn->query($content);
-//$row = $result_db->fetch_assoc();
-$field_cnt = $result_db->field_count;
-$row_cnt = $result_db->num_rows;
+$presname_db = $username . "_" . $presname_db;
 
+$rowcounterfetch = "SELECT * FROM `$presname_db`";
+$rowresult = $conn->query($rowcounterfetch);
+$row_cnt = $rowresult->num_rows;
+$rowresult->close();
 
-echo " is the name and the table has ".$field_cnt." fields .";
-$content_fields = $field_cnt-1;
-echo "However I only need the fields with actual content which is: ".$content_fields. " fields. The last field is just the ID <br />";
+$graphcolor = array("backgroundColor: color(window.chartColors.red).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.orange).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.yellow).alpha(1).rgbString(),", "backgroundColor: color(window.chartColors.green).alpha(1).rgbString(),");
+$bordercolor = array("borderColor: window.chartColors.red,", "borderColor: window.chartColors.orange,", "borderColor: window.chartColors.yellow,", "borderColor: window.chartColors.green,");
 
-$store_array = array();
-while($row = $result_db->fetch_assoc()) {
-    //$store_array[] = $row;
-    print_r($row);
-}
+$sql = "SELECT * FROM `$presname_db` WHERE id = '1'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$counter_array = array_filter($row, 'strlen');
 
-/*
-echo "<br /> HERE IS ROW NUMBER 2 <br />";
-echo $row["2"];
-*/
-/*
-for ($set = array (); $row = $result_db->fetch_assoc(); $set[array_shift($row)] = $row);
-print_r($set);
-*/
-
+$field_cnt = count($counter_array);
+$field_cnt = $field_cnt - $non_value_fields;
+$result->close();
 ?>
 
+<head>
+    <script src="vendor/nnnick/chartjs/dist/Chart.bundle.js"></script>
+    <script src="vendor/nnnick/chartjs/samples/utils.js"></script>
+    <style>
+        canvas {
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+        }
+    </style>
+</head>
+<body>
+<br/><br/><br/><br/><br/>
+<H6 class="text-center">Presentation title</H6>
+<br/><br/><br/>
+<?php
+$num = 0;
+
+for ($i = 0; $i < $row_cnt; $i++) {
+    $idsrc = $i + 1;
+    $sql = "SELECT * FROM `$presname_db` WHERE id = '$idsrc'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $counter_array = array_filter($row, 'strlen');
+
+    $field_cnt = count($counter_array);
+    $field_cnt = $field_cnt - $non_value_fields;
+    $result->close();
+
+    if ($num % 2 == 0) {
+        echo '<div class="container-fluid bg-3 text-center">
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-5 text-right">
+            <h1>';
+        echo $row["1"];
+        echo '</h1>
+            <P>FILMED IN NEVADA</P><br/>
+            <span class="glyphicon glyphicon-search" aria-hidden="true" style="font-size:250%;"></span>
+            <div id="container">
+                <canvas id="chart' . $i . '"></canvas>
+            </div>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, donec volutpat egestas eget egestas sed porttitor, nulla nonummy nec ut,
+                nulla vel ultricies amet, ac lectus consequat velit tempor, quam vestibulum vitae. Sit nullam. Veniam
+                justo nunc porttitor magna sed ante, mi nulla orci odio eros id nullam, cras bibendum feugiat. At
+                vestibulum donec massa purus ut augue, mauris sed magna ipsum omnis amet, vestibulum laoreet dolor,
+                aliquam ipsum risus tempus ut eros, fusce purus urna ullamcorper lobortis</p>
+
+        </div>';
+    }
+
+    if ($num % 2 == 1) {
+        echo '<div class="col-sm-5 text-right">
+            <h1>';
+        echo $row["1"];
+        echo '</h1>
+            <P>FILMED IN NEVADA</P><br/>
+            <span class="glyphicon glyphicon-search" aria-hidden="true" style="font-size:250%;"></span>
+            <div id="container">
+                <canvas id="chart' . $i . '"></canvas>
+            </div>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, donec volutpat egestas eget egestas sed porttitor, nulla nonummy nec ut,
+                nulla vel ultricies amet, ac lectus consequat velit tempor, quam vestibulum vitae. Sit nullam. Veniam
+                justo nunc porttitor magna sed ante, mi nulla orci odio eros id nullam, cras bibendum feugiat. At
+                vestibulum donec massa purus ut augue, mauris sed magna ipsum omnis amet, vestibulum laoreet dolor,
+                aliquam ipsum risus tempus ut eros, fusce purus urna ullamcorper lobortis</p>
+
+        </div>
+        <div class="col-sm-1"></div> 
+    </div>
+</div>';
+
+
+        $num++;
+    }
+
+    echo '<script>';
+
+    echo 'var color = Chart.helpers.color;';
+
+    echo 'var barChartData'.$i.' = {';
+
+    echo 'labels: [""],';
+    echo 'datasets: [{';
+    for ($s = 0; $s < $field_cnt; $s++) {
+        $index = $s + 2;
+        echo 'label: "Row ' . $s . '",';
+        echo $graphcolor[$s % 4];
+        echo 'borderColor: window.chartColors.red,';
+        echo 'borderWidth: 0,';
+        echo 'data: [';
+        echo $row["$index"];
+        echo ']';
+        echo '},';
+
+        if ($s != $field_cnt - 1) {
+            echo '{';
+        }
+    }
+
+    echo ']';
+    echo '};';
+
+
+    echo 'var ctx = document.getElementById("chart'.$i.'");';
+    echo 'var chart'.$i.' = new Chart(ctx, {';
+    echo 'type: "bar",';
+    echo 'data: barChartData'.$i.',';
+    echo 'options: {';
+    echo 'scales: {';
+    echo 'xAxes: [{';
+    echo'    display: false';
+
+    echo '}],';
+    echo 'yAxes: [{';
+
+    echo 'display: false';
+
+    echo '}]';
+    echo '},';
+    echo 'legend: {';
+    echo 'display: false';
+    echo '}';
+    echo '}';
+    echo '})';
+    echo'</script>'; } ?>
 </body>
+
 </html>

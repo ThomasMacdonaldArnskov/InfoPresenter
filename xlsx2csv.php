ffa302215f62e8802e6a5437fdc82fa54d68dad5
jlitvi14 <jlitvi14@student.aau.dk>
2017-05-14 15:43:35 +0200

    Quick directory cleanup

--- xlsx2csv.php
+++ xlsx2csv.php
@@ -10,13 +10,13 @@ $excel_readers = array(
 require_once('PHPExcel/PHPExcel.php');
 $reader = PHPExcel_IOFactory::createReader('Excel5');
 $reader->setReadDataOnly(true);
-$path = 'xlsxTestOne.xls';
+$path = 'xlsxToCsv/convertThisFile.xls';
 $excel = $reader->load($path);
 $writer = PHPExcel_IOFactory::createWriter($excel, 'CSV')
                                       ->setEnclosure('')
                                       ->setLineEnding("\r\n")
                                       ->setDelimiter(';');
 ;
-$writer->save('itWorks.csv');
+$writer->save('xlsxToCsv/convertThisFile_converted.csv');
 
 ?>

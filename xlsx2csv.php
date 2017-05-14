a7bd12831d8d39fa8e9577e2495db165677bb0a2
jlitvi14 <jlitvi14@student.aau.dk>
2017-05-14 15:18:34 +0200

    xlsx -> csv conversion works
    just need to pimp it now

---
+++ xlsx2csv.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+    Convert excel file to csv
+*/
+$excel_readers = array(
+    'Excel5' , 
+    'Excel2003XML' , 
+    'Excel2007'
+);
+require_once('PHPExcel/PHPExcel.php');
+$reader = PHPExcel_IOFactory::createReader('Excel5');
+$reader->setReadDataOnly(true);
+$path = 'xlsxTestOne.xls';
+$excel = $reader->load($path);
+$writer = PHPExcel_IOFactory::createWriter($excel, 'CSV')
+                                      ->setEnclosure('')
+                                      ->setLineEnding("\r\n")
+                                      ->setDelimiter(';');
+;
+$writer->save('itWorks.csv');
+
+?>
